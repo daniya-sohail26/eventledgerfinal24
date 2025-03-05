24039e874460eb1eb2de059ee22a5dae3084c017
KanzaAkram <kanzaakram123@gmail.com>
2025-03-06 00:37:34 +0500

    landing page done

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -90,7 +90,7 @@ export const BentoCard = ({ src, title, description, isComingSoon }) => {
               }}
             />
             <TiLocationArrow className="relative z-20" />
-            <p className="relative z-20">coming soon</p>
+            <p className="relative z-20">book tickets now</p>
           </div>
         )}
       </div>
@@ -99,28 +99,28 @@ export const BentoCard = ({ src, title, description, isComingSoon }) => {
 };
 
 const Features = () => (
-  <section className="bg-black pb-52">
+  <section id ="features" className="bg-black pb-52">
     <div className="container mx-auto px-3 md:px-10">
       <div className="px-5 py-32">
         <p className="font-circular-web text-lg text-blue-50">
-          Into the Metagame Layer
+          <b>Explore Features</b>
         </p>
         <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
-          Immerse yourself in a rich and ever-expanding universe where a vibrant
-          array of products converge into an interconnected overlay experience
-          on your world.
+          Step into the future of event management with Event Ledger—a
+          decentralized platform integrating NFT ticketing, blockchain-based
+          validation, and transparent revenue management.
         </p>
       </div>
 
       <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
         <BentoCard
-          src="videos/feature-1.mp4"
+          src="videos/validation.mp4"
           title={
             <>
-              radia<b>n</b>t
+              Decentralized<b> Validatio</b>n
             </>
           }
-          description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
+          description="Ensures event hosts are verified through blockchain-powered document authentication, reducing fraud risks."
           isComingSoon
         />
       </BentoTilt>
@@ -128,60 +128,68 @@ const Features = () => (
       <div className="grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
         <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
           <BentoCard
-            src="videos/feature-2.mp4"
+            src="videos/nft.mp4"
             title={
               <>
-                zig<b>m</b>a
+                NFT Tick<b>etin</b>g
               </>
             }
-            description="An anime and gaming-inspired NFT collection - the IP primed for expansion."
+            description="Issues tamper-proof NFT-based tickets with ownership verification and seamless resale capabilities.
+
+"
             isComingSoon
           />
         </BentoTilt>
 
         <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
           <BentoCard
-            src="videos/feature-3.mp4"
+            src="videos/hero-1.mp4"
             title={
               <>
-                n<b>e</b>xus
+                Transparent Market<b>pla</b>ce
               </>
             }
-            description="A gamified social hub, adding a new dimension of play to social interaction for Web3 communities."
+            description="Allows attendees to resell tickets with automated pricing, eliminating middlemen and fraud."
             isComingSoon
           />
         </BentoTilt>
 
         <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
           <BentoCard
-            src="videos/feature-4.mp4"
+            src="videos/payment.mp4"
             title={
               <>
-                az<b>u</b>l
+                Secure Paym<b>ent</b>s
               </>
             }
-            description="A cross-world AI Agent - elevating your gameplay to be more fun and productive."
+            description="Integrates MetaMask for decentralized, secure, and efficient ticket purchases and transactions."
             isComingSoon
           />
         </BentoTilt>
 
         <BentoTilt className="bento-tilt_2">
-          <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
-            <h1 className="bento-title special-font max-w-64 text-black">
-              M<b>o</b>re co<b>m</b>ing s<b>o</b>on.
-            </h1>
-
-            <TiLocationArrow className="m-5 scale-[5] self-end" />
-          </div>
+          <BentoCard
+            src="videos/ipfs.mp4"
+            title={
+              <>
+                Immutable <b>Data </b>Storage
+              </>
+            }
+            description="Uses IPFS and Ethereum smart contracts to store and validate event data securely and transparently."
+            isComingSoon
+          />
         </BentoTilt>
 
         <BentoTilt className="bento-tilt_2">
-          <video
-            src="videos/feature-5.mp4"
-            loop
-            muted
-            autoPlay
-            className="size-full object-cover object-center"
+          <BentoCard
+            src="videos/multiportal.mp4"
+            title={
+              <>
+                Multi <b>-Portal </b>Access
+              </>
+            }
+            description="Provides tailored portals for event hosts, and attendees, enhancing user experience."
+            isComingSoon
           />
         </BentoTilt>
       </div>
